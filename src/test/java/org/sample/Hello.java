d3178e943cf12dbe3c147ae88f5547db7b10abc8
karthirajan <kajekarthi@gmail.com>
2019-02-08 13:54:03 +0530

    added lists

--- src/test/java/org/sample/Hello.java
+++ src/test/java/org/sample/Hello.java
@@ -1,5 +1,7 @@
 package org.sample;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +11,7 @@ import org.openqa.selenium.support.ui.Select;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 import junit.framework.Assert;
 
 public class Hello {
@@ -26,27 +29,30 @@ public void the_user_click_register_to_create_the_form() {
     
 }
 
+
+
 @When("The user-one fill in the form details")
-public void the_user_one_fill_in_the_form_details() {
-    d.findElement(By.id("user_firstname")).sendKeys("karthi");
-d.findElement(By.id("user_surname")).sendKeys("rajan");
-d.findElement(By.id("user_phone")).sendKeys("95768956");
-WebElement dob = d.findElement(By.id("user_dateofbirth_1i"));
-Select s=new Select(dob);
- s.selectByValue("1994");
- WebElement dob1 = d.findElement(By.id("user_dateofbirth_2i"));
-	Select s1=new Select(dob1);
-  s1.selectByValue("6");
-  WebElement dob2 = d.findElement(By.id("user_dateofbirth_3i"));
-	Select s2=new Select(dob2);
-   s2.selectByValue("7");
-d.findElement(By.id("user_address_attributes_street")).sendKeys("tnjre");
-d.findElement(By.id("user_address_attributes_city")).sendKeys("tnjre");
-d.findElement(By.id("user_address_attributes_county")).sendKeys("india");
-d.findElement(By.id("user_address_attributes_postcode")).sendKeys("614-804");
-d.findElement(By.id("user_user_detail_attributes_email")).sendKeys("kajekarthi@gmail.com");
-d.findElement(By.id("user_user_detail_attributes_password")).sendKeys("karthi1007");
-d.findElement(By.id("user_user_detail_attributes_password_confirmation")).sendKeys("karthi1007");
+public void the_user_one_fill_in_the_form_details(DataTable dataTable) {
+	List<String> insDetails = dataTable.asList();
+	d.findElement(By.id("user_firstname")).sendKeys(insDetails.get(0));
+	  d.findElement(By.id("user_surname")).sendKeys(insDetails.get(1));
+	  d.findElement(By.id("user_phone")).sendKeys(insDetails.get(2));
+	  WebElement dob = d.findElement(By.id("user_dateofbirth_1i"));
+	  Select s=new Select(dob);
+	   s.selectByValue("1994");
+	   WebElement dob1 = d.findElement(By.id("user_dateofbirth_2i"));
+	  	Select s1=new Select(dob1);
+	    s1.selectByValue("6");
+	    WebElement dob2 = d.findElement(By.id("user_dateofbirth_3i"));
+	  	Select s2=new Select(dob2);
+	     s2.selectByValue("7");
+	  d.findElement(By.id("user_address_attributes_street")).sendKeys(insDetails.get(3));
+	  d.findElement(By.id("user_address_attributes_city")).sendKeys(insDetails.get(4));
+	  d.findElement(By.id("user_address_attributes_county")).sendKeys(insDetails.get(5));
+	  d.findElement(By.id("user_address_attributes_postcode")).sendKeys(insDetails.get(6));
+	  d.findElement(By.id("user_user_detail_attributes_email")).sendKeys(insDetails.get(7));
+	  d.findElement(By.id("user_user_detail_attributes_password")).sendKeys(insDetails.get(8));
+	  d.findElement(By.id("user_user_detail_attributes_password_confirmation")).sendKeys(insDetails.get(9));
 }
 
 @When("The user-one submits the form")
